723b1e09ee85f8fefa79ba5d312351070bb2ebc9
olaulau <olaulau@gmail.com>
2023-07-03 16:53:45 +0200

    integrate graph in repository view, moved data prepare into controller

--- php/controller/RepositoryCtrl.php
+++ php/controller/RepositoryCtrl.php
@@ -1,6 +1,8 @@
 <?php
 namespace controller;
 
+use olafnorge\borgphp\ListCommand;
+
 class RepositoryCtrl
 {
 
@@ -23,8 +25,47 @@ class RepositoryCtrl
 	}
 	
 	
-	public static function viewGET ()
+	public static function viewGET ($f3)
 	{
+		require __DIR__ . '/../../config.inc.php';
+
+		$location = $_GET["location"];
+		$f3->set("location", $location);
+		$repo_name = array_search($location, $conf["repos"]);
+		$f3->set("repo_name", $repo_name);
+		
+		// list repository's archives
+		$cmd = new ListCommand([
+			$location,
+		]);
+		
+		$cmd->setEnv([
+			"BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK" => "yes",
+		]);
+		try
+		{
+			$output = $cmd->mustRun()->getOutput();
+		}
+		catch (Exception $e)
+		{
+			echo "<pre>" . $e->getMessage() . "</pre>";
+			echo "<hr>";
+			$err = $cmd->getErrorOutput();
+			echo "<pre>"; var_dump($err); echo "</pre>";
+			die;
+		}
+		// 	var_dump($output); die;
+		$f3->set("output", $output);
+		
+		$js_data = [];
+		foreach ($output["archives"] as $archive)
+		{
+			$dt = new \DateTime($archive["start"]);
+			$js_data [] = $dt->getTimestamp();
+		}
+		// var_dump($js_data); die;
+		$f3->set("js_data", $js_data);
+		
 		$view = new \View();
 		echo $view->render('repository.phtml');
 	}
