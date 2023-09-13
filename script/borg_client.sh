4777268533a9f78dc10f973c82faeb100f93e806
olaulau <olaulau@gmail.com>
2023-09-13 10:45:07 +0200

    also updated borg_client script

--- script/borg_client.sh
+++ script/borg_client.sh
@@ -15,14 +15,14 @@ else
     exit
 fi
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
     echo "invalid parameter count."
     exit 1
 fi
 server_name="$1"
-repo_name="$2"
-remote_user_name=$repo_name
+user_name="$2"
+repo_name="$3"
 config_file=`realpath ~/.config/borgmatic/borgmatic_$server_name.yaml`
 
 #validate-borgmatic-config --config $config_file
@@ -35,11 +35,11 @@ borgmatic --config $config_file check  --verbosity 1 --progress
 borgmatic --config $config_file list
 borgmatic --config $config_file info
 
-ssh $remote_user_name@$server_name "chmod -R u+rwx,g+rwxs,o-rwx /home/$remote_user_name/borg/"
+ssh $user_name@$server_name "chmod -R u+rwx,g+rwxs,o-rwx /home/$user_name/borg/$repo_name/"
 
-url="https://$server_name/borg-ui/cache/update/$repo_name"
+url="https://$server_name/borg-ui/cache/update/$user_name/$repo_name"
 curl $url
-url="https://$server_name/borg-ui_DEV/cache/update/$repo_name"
+url="https://$server_name/borg-ui_DEV/cache/update/$user_name/$repo_name"
 curl $url
 
 echo "end : `date`"
